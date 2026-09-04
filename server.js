ca1b58eb6de39b39ee521a63775b3afc0a8fe44a
Marco Antônio <marco.silva.dev2024@gmail.com>
2026-09-03 21:48:54 -0300

    mudança de porta para deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const FILE = 'data.json';
 
 // Permite receber JSON
